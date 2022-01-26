libbpf/bpftool

commit 28b7cccadccea4ae18de4bad8fda9f5706dfe24c

mirror: Reimplement feature detection and reallocarray()
Implement feature detection for features and libraries that bpftool
requires for its core or its optional features. These are:

  - Availability of clang, BPF, CO-RE for building skeletons
  - Libbfd
  - Libcap
  - The definition of 'disassembler()', which changed at some point in
    binutils' bfd/opcodes
  - Zlib

Unconditionally reimplement reallocarray() to avoid the pain of testing
the libc version. The implementation is copied from libbpf's version.

Remove target "feature-detect-clean" from the "clean" target in
bpftool's Makefile, to prevent make from changing to
tools/build/feature/, which does not exist in this mirror.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/Makefile.feature
@@ -1,0 +1,105 @@
+# SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+
+pound := \#
+
+### feature-clang-bpf-co-re
+
+feature-clang-bpf-co-re := \
+  $(shell echo 'struct s { int i; } __attribute__((preserve_access_index)); struct s foo;' | \
+    $(CLANG) -g -target bpf -S -o - -x c - 2>/dev/null | grep -q BTF_KIND_VAR && echo 1)
+
+### feature-reallocarray
+
+# This mirror re-implements reallocarray(), so the function is always available.
+feature-reallocarray := 1
+
+### feature-libbfd
+
+LIBBFD_PROBE := '$(pound)include <bfd.h>\n'
+LIBBFD_PROBE += 'int main(void) {'
+LIBBFD_PROBE += '	bfd_demangle(0, 0, 0);'
+LIBBFD_PROBE += '	return 0;'
+LIBBFD_PROBE += '}'
+
+define libbfd_build
+  $(shell echo $(LIBBFD_PROBE) | \
+    $(CC) $(CFLAGS) -Wall -Werror $(LDFLAGS) -x c - $(1) -S -o - >/dev/null 2>&1 \
+    && echo 1)
+endef
+
+feature-libbfd := \
+  $(call libbfd_build,-lbfd -ldl)
+ifneq ($(feature-libbfd),1)
+  feature-libbfd-liberty := \
+    $(call libbfd_build,-lbfd -ldl -liberty)
+  ifneq ($(feature-libbfd-liberty),1)
+    feature-libbfd-liberty-z := \
+      $(call libbfd_build,-lbfd -ldl -liberty -lz)
+  endif
+endif
+HAS_LIBBFD := $(findstring 1, \
+  $(feature-libbfd)$(feature-libbfd-liberty)$(feature-libbfd-liberty-z))
+
+### feature-disassembler-four-args
+
+DISASSEMBLER_PROBE := '$(pound)include <dis-asm.h>\n'
+DISASSEMBLER_PROBE += 'int main(void) {'
+DISASSEMBLER_PROBE += '	disassembler((enum bfd_architecture)0, 0, 0, NULL);'
+DISASSEMBLER_PROBE += '	return 0;'
+DISASSEMBLER_PROBE += '}'
+
+define disassembler_build
+  $(shell echo $(DISASSEMBLER_PROBE) | \
+    $(CC) $(CFLAGS) -Wall -Werror $(LDFLAGS) -x c - -lbfd -lopcodes -S -o - >/dev/null 2>&1 \
+    && echo 1)
+endef
+
+feature-disassembler-four-args := $(findstring 1, $(call disassembler_build))
+
+### feature-zlib
+
+ZLIB_PROBE := '$(pound)include <zlib.h>\n'
+ZLIB_PROBE += 'int main(void) {'
+ZLIB_PROBE += '	gzopen(NULL, "r");'
+ZLIB_PROBE += '	return 0;'
+ZLIB_PROBE += '}'
+
+define zlib_build
+  $(shell echo $(ZLIB_PROBE) | \
+    $(CC) $(CFLAGS) -Wall -Werror $(LDFLAGS) -x c - -lz -S -o - >/dev/null 2>&1 \
+    && echo 1)
+endef
+
+feature-zlib := $(findstring 1, $(call zlib_build))
+
+### feature-libcap
+
+LIBCAP_PROBE := '$(pound)include <sys/capability.h>\n'
+LIBCAP_PROBE += 'int main(void) {'
+LIBCAP_PROBE += '	cap_free(0);'
+LIBCAP_PROBE += '	return 0;'
+LIBCAP_PROBE += '}'
+
+define libcap_build
+  $(shell echo $(LIBCAP_PROBE) | \
+    $(CC) $(CFLAGS) -Wall -Werror $(LDFLAGS) -x c - -lcap -S -o - >/dev/null 2>&1 \
+    && echo 1)
+endef
+
+feature-libcap := $(findstring 1, $(call libcap_build))
+
+### Print detection results
+
+define print_status
+  ifeq ($(1), 1)
+    MSG = $(shell printf '...%30s: [ \033[32mon\033[m  ]' $(2))
+  else
+    MSG = $(shell printf '...%30s: [ \033[31mOFF\033[m ]' $(2))
+  endif
+endef
+feature_print_status = $(eval $(print_status)) $(info $(MSG))
+
+$(call feature_print_status,$(HAS_LIBBFD),libbfd)
+
+$(foreach feature,$(filter-out libbfd,$(FEATURE_DISPLAY)), \
+  $(call feature_print_status,$(feature-$(feature)),$(feature)))

include/tools/libc_compat.h
@@ -1,0 +1,48 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef __TOOLS_LIBC_COMPAT_H
+#define __TOOLS_LIBC_COMPAT_H
+
+#include <limits.h>
+#include <stdlib.h>
+
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#ifndef __has_builtin
+#define __has_builtin(x) 0
+#endif
+
+/*
+ * Re-implement glibc's reallocarray(). This function is not available in all
+ * versions of glibc. The version of bpftool shipped with the kernel deals with
+ * this by doing an extra feature detection, and by using a stub in
+ * <tools/libc_compat.h> (this file) when COMPAT_NEED_REALLOCARRAY is set.
+ * Let's keep things simple here: it is trivial to re-implement the function.
+ * Libbpf does the same in libbpf_internal.h: we can copy its version.
+ */
+static inline void *bpftool_reallocarray(void *ptr, size_t nmemb, size_t size)
+{
+	size_t total;
+
+#if __has_builtin(__builtin_mul_overflow)
+	if (unlikely(__builtin_mul_overflow(nmemb, size, &total)))
+		return NULL;
+#else
+	if (size == 0 || nmemb > ULONG_MAX / size)
+		return NULL;
+	total = nmemb * size;
+#endif
+	return realloc(ptr, total);
+}
+
+/*
+ * Overwrite default reallocarray(). It would probably be cleaner to use
+ * "bpftool_reallocarray()" in the source code to make the distinction
+ * explicit, but we want to avoid touching the code to remain in sync with the
+ * kernel and ease maintenance.
+ */
+#define reallocarray(ptr, nmemb, size) bpftool_reallocarray(ptr, nmemb, size)
+
+#endif

src/Makefile
@@ -109,11 +109,7 @@ endif
 endif
 
 ifeq ($(check_feat),1)
-ifeq ($(FEATURES_DUMP),)
-include $(srctree)/tools/build/Makefile.feature
-else
-include $(FEATURES_DUMP)
-endif
+include Makefile.feature
 endif
 
 ifeq ($(feature-disassembler-four-args), 1)
@@ -231,7 +227,7 @@ feature-detect-clean:
 	$(call QUIET_CLEAN, feature-detect)
 	$(Q)$(MAKE) -C $(srctree)/tools/build/feature/ clean >/dev/null
 
-clean: $(LIBBPF)-clean $(LIBBPF_BOOTSTRAP)-clean feature-detect-clean
+clean: $(LIBBPF)-clean $(LIBBPF_BOOTSTRAP)-clean
 	$(call QUIET_CLEAN, bpftool)
 	$(Q)$(RM) -- $(OUTPUT)bpftool $(OUTPUT)*.o $(OUTPUT)*.d
 	$(Q)$(RM) -- $(OUTPUT)*.skel.h $(OUTPUT)vmlinux.h

commit 706c0e9bb999681320c17eec549fddb59f922640

mirror: Fix features detection for building with clang
When detecting the features on the machine, clang does not need the
flags for telling the linker what libraries to use. As a consequence, it
prints a warning to let users know that the flag is unused, and this
turns into an error due to the use of -Wall and -Werror:

  clang: error: -: 'linker' input unused [-Werror,-Wunused-command-line-argument]

To address it, we can temporarily add -Wno-unused-command-line-argument
to the $(CFLAGS) when we check the features with clang.

Fixes: #3
Fixes: 28b7cccadcce ("mirror: Reimplement feature detection and reallocarray()")

Reported-by: @aguaiyoung
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/Makefile.feature
@@ -2,6 +2,11 @@
 
 pound := \#
 
+CFLAGS_BACKUP := $(CFLAGS)
+ifneq ($(LLVM),)
+  CFLAGS += -Wno-unused-command-line-argument
+endif
+
 ### feature-clang-bpf-co-re
 
 feature-clang-bpf-co-re := \
@@ -103,3 +108,5 @@ $(call feature_print_status,$(HAS_LIBBFD),libbfd)
 
 $(foreach feature,$(filter-out libbfd,$(FEATURE_DISPLAY)), \
   $(call feature_print_status,$(feature-$(feature)),$(feature)))
+
+CFLAGS := $(CFLAGS_BACKUP)