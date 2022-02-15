d5a59b22016ac284a0892f0e0b3898ab524cfd23
James Hilliard <james.hilliard1@gmail.com>
2022-02-15 15:02:53 -0700

    mirror: Ship linux/netlink.h UAPI header
    Copy tools/include/uapi/linux/netlink.h from kernel's repository, taken
    at commit dc37dc617fab ("libbpf: Fix compilation warning due to
    mismatched printf format") (current checkpoint).
    
    We want this header in the repo so we can be sure that it contains the
    flags NETLINK_EXT_ACK, used in net.c.
    
    Fixes: #10
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- scripts/sync-kernel.sh
+++ scripts/sync-kernel.sh
@@ -60,6 +60,7 @@ PATH_MAP=(									\
 	[tools/include/uapi/linux/btf.h]=include/uapi/linux/btf.h		\
 	[tools/include/uapi/linux/const.h]=include/uapi/linux/const.h		\
 	[tools/include/uapi/linux/if_link.h]=include/uapi/linux/if_link.h	\
+	[tools/include/uapi/linux/netlink.h]=include/uapi/linux/netlink.h	\
 	[tools/include/uapi/linux/perf_event.h]=include/uapi/linux/perf_event.h	\
 	[tools/include/uapi/linux/pkt_cls.h]=include/uapi/linux/pkt_cls.h	\
 	[tools/include/uapi/linux/pkt_sched.h]=include/uapi/linux/pkt_sched.h	\
