libbpf/bpftool

commit 5f7101c7bee9382d6229a77a6fb258ac7424a577

mirror: Fix libbfd feature detection by trimming $(feature-libbfd*)
Feature detection for libbfd is currently broken in such a way that the
result is not passed down and accounted for when building bpftool, even
if the detection itself was successful (and displayed as such by the
Makefile).

In libbpf_build, the indent at the beginning of the definition (before
"$(shell ...") is interpreted by make as part of the result of the
$(call), making the final $(feature-libbfd), or its derivatives, equal
to "  1" and not to "1" when the feature is detected.

As a result, the strict comparison "ifeq ($(feature-libbfd),1)" in the
main Makefile fails, and the result is not passed down properly when
building the tool.

Fix it just as we did for the other $(feature-*) variables: call
$(findstring 1,...) on the result, to just keep the "1" if it is
present.

Fixes: 28b7cccadcce ("mirror: Reimplement feature detection and reallocarray()")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/Makefile.feature
@@ -28,13 +28,13 @@ define libbfd_build
 endef
 
 feature-libbfd := \
-  $(call libbfd_build,-lbfd -ldl)
+  $(findstring 1,$(call libbfd_build,-lbfd -ldl))
 ifneq ($(feature-libbfd),1)
   feature-libbfd-liberty := \
-    $(call libbfd_build,-lbfd -ldl -liberty)
+    $(findstring 1,$(call libbfd_build,-lbfd -ldl -liberty))
   ifneq ($(feature-libbfd-liberty),1)
-    feature-libbfd-liberty-z := \
-      $(call libbfd_build,-lbfd -ldl -liberty -lz)
+    $(findstring 1,feature-libbfd-liberty-z := \
+      $(call libbfd_build,-lbfd -ldl -liberty -lz))
   endif
 endif
 HAS_LIBBFD := $(findstring 1, \

commit 1558d32385ac8e10c20831182dbe9a04d82c93e1

mirror: Fix Makefile error for feature-libbfd-liberty-z
When attempting to strengthen libbfd feature detection in
Makefile.feature, the fix was not applied properly to
feature-libbfd-liberty-z, instead breaking feature detection for the
case when libbfd is not installed. Let's repair it.

Closes: #23
Fixes: 5f7101c7bee9 ("mirror: Fix libbfd feature detection by trimming $(feature-libbfd*)")
Reported-by: Lam Thai
Co-authored-by: Lam Thai
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

src/Makefile.feature
@@ -33,8 +33,8 @@ ifneq ($(feature-libbfd),1)
   feature-libbfd-liberty := \
     $(findstring 1,$(call libbfd_build,-lbfd -ldl -liberty))
   ifneq ($(feature-libbfd-liberty),1)
-    $(findstring 1,feature-libbfd-liberty-z := \
-      $(call libbfd_build,-lbfd -ldl -liberty -lz))
+    feature-libbfd-liberty-z := \
+      $(findstring 1,$(call libbfd_build,-lbfd -ldl -liberty -lz))
   endif
 endif
 HAS_LIBBFD := $(findstring 1, \