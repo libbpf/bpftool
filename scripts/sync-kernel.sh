libbpf/bpftool

commit cc5caf6cee812aaf47e1c39da8c249c9cd41d2ff

mirror: Track dis-asm-compat.h from upstream
Following a change in the initialisation function in the disassembler,
bpftool needs a compatibility snippet that was added in kernel commit
a45b3d692623 ("tools include: add dis-asm-compat.h to handle version
differences"). We need to synchronise this file in this repository to be
able to compile bpftool with recent releases of libbfd.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

scripts/sync-kernel.sh
@@ -53,6 +53,7 @@ PATH_MAP=(									\
 	[${BPFTOOL_SRC_DIR}/Documentation]=docs					\
 	[kernel/bpf/disasm.c]=src/kernel/bpf/disasm.c				\
 	[kernel/bpf/disasm.h]=src/kernel/bpf/disasm.h				\
+	[tools/include/tools/dis-asm-compat.h]=include/tools/dis-asm-compat.h	\
 	[tools/include/uapi/asm-generic/bitsperlong.h]=include/uapi/asm-generic/bitsperlong.h	\
 	[tools/include/uapi/linux/bpf_common.h]=include/uapi/linux/bpf_common.h	\
 	[tools/include/uapi/linux/bpf.h]=include/uapi/linux/bpf.h		\

commit 1f7ffe5f27b5e2452d273f9ecd2910e8bee659c7

mirror: Fix sync-up script regarding dis-asm-compat.h handling
When syncing, we need to create the temporary target directory
__bpftool/include/tools/ so we can copy the compat header file in there.

Fixes: cc5caf6cee81 ("mirror: Track dis-asm-compat.h from upstream")
Signed-off-by: Quentin Monnet <quentin@isovalent.com>

scripts/sync-kernel.sh
@@ -81,7 +81,7 @@ BPFTOOL_TREE_FILTER+="git mv -kf ${BPFTOOL_SRC_DIR} __bpftool/${PATH_MAP[${BPFTO
 BPFTOOL_TREE_FILTER+="git mv -kf __bpftool/src/bash-completion __bpftool/bash-completion && "$'\\\n'
 BPFTOOL_TREE_FILTER+="git mv -kf __bpftool/src/Documentation __bpftool/docs && "$'\\\n'
 
-BPFTOOL_TREE_FILTER+="mkdir -p __bpftool/include/uapi/asm-generic __bpftool/include/uapi/linux/tc_act __bpftool/src/kernel/bpf && "$'\\\n'
+BPFTOOL_TREE_FILTER+="mkdir -p __bpftool/include/tools __bpftool/include/uapi/asm-generic __bpftool/include/uapi/linux/tc_act __bpftool/src/kernel/bpf && "$'\\\n'
 for p in "${!PATH_MAP[@]}"; do
 	case ${p} in
 		${BPFTOOL_SRC_DIR}*)